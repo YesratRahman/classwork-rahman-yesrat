77c7b31a63481f6b87d0986e1736509d85633cfd
YesratRahman <yr2818@vt.edu>
2021-02-10 15:56:12 -0500

    testing dao

--- UserMoneyManager/src/test/java/com/tp/UserMoneyManager/daos/PostgresUserDaoTests.java
+++ UserMoneyManager/src/test/java/com/tp/UserMoneyManager/daos/PostgresUserDaoTests.java
@@ -1,4 +1,27 @@
 package com.tp.UserMoneyManager.daos;
 
+import org.checkerframework.checker.units.qual.A;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("daoTesting")
 public class PostgresUserDaoTests {
+
+    @Autowired
+    UserPostgresDao toTest;
+
+    @Autowired
+    JdbcTemplate template;
+
+    @BeforeEach
+    public void setUp(){
+       // template.update("TRUNCATE ")
+    }
+
+
 }

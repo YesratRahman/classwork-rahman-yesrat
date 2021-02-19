f48b0735e6fb3fa524c3f32147d68e7e18cb98e8
YesratRahman <yr2818@vt.edu>
2021-02-19 15:28:54 -0500

    can generate random cat pic

--- CAT/cat.js
+++ CAT/cat.js
@@ -1,4 +1,25 @@
 
+let generate_btn = document.querySelector(".generate_button"); 
+generate_btn.addEventListener("click", getPictures); 
+function getPictures(){
+    let catImgDiv = document.querySelector(".catsImages")
+    catImgDiv.innerHTML = ''
+    fetch('https://api.thecatapi.com/v1/images/search')
+    .then(response => response.json())
+    .then((data) => {
+       let catsImageUrl = data[0].url
+
+       let catImageElement = document.createElement("img")
+       catImageElement.setAttribute('src', `${catsImageUrl}`)
+       catImageElement.classList.add("imageResize")
+       let catImgDiv = document.querySelector(".catsImages")
+       catImgDiv.append(catImageElement)
+    })
+    .catch(err=> console.log(err))
+
+}
+
+
 
 const getBreed = function() {
 
@@ -12,14 +33,27 @@ dropdown.prop('selectedIndex', 0);
 const url = 'https://api.thecatapi.com/v1/breeds?2280658c27df8c0709b6a8c33e204b72';
 
 $.getJSON(url, function (data) {
-  $.each(data, function (key, entry) {
-    dropdown.append($('<option></option>').attr('value', entry.id).text(entry.name));
+  $.each(data, function (key, value) {
+    dropdown.append($('<option></option>').attr('value', value.id).text(value.name));
+
+    $("#lifeSpan").attr('value', key).text(value.life_span);
+   // $( "<img>" ).attr( "src", value.image ).appendTo( "#images" );
+
 
    // $("#reportHeader").text(`Information for ${entry.name}` );
-   // $("#lifeSpan").text(data.life_span);
+  //  $("#lifeSpan").attr('value', key).text(entry.life_span);
+  //$('#lifeSpan').val(entry.life_span);
 
-
-
-  })
+  });
+  console.log( data );
+           
 })
 } ;
+
+
+
+
+
+
+getBreed(); 
+

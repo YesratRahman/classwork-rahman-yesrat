f9a21884f902ce1654b6fae3577f46a21f046b47
YesratRahman <yr2818@vt.edu>
2021-02-19 22:05:49 -0500

    get breed by id working

--- CAT/cat.js
+++ CAT/cat.js
@@ -19,41 +19,67 @@ function getPictures(){
 
 }
 
+let myKey = "2280658c27df8c0709b6a8c33e204b72";
 
 
-const getBreed = function() {
+let currentBreeds = "";
 
-let dropdown = $('#breed-dropdown');
+function getBreeds() {
+    let breedUrl = "https://api.thecatapi.com/v1/breeds?attach_breed=0&api_key=" + myKey;
+    fetch(breedUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                for (let i = 0; i < data.length; i++) {
+                    let breedOptions = $("<option>").val(data[i].id).text(data[i].name);
 
-dropdown.empty();
+                    $("#breed").append(breedOptions);
+                }
 
-dropdown.append('<option selected="true" disabled>Choose Breed</option>');
-dropdown.prop('selectedIndex', 0);
-
-const url = 'https://api.thecatapi.com/v1/breeds?2280658c27df8c0709b6a8c33e204b72';
-
-$.getJSON(url, function (data) {
-  $.each(data, function (key, value) {
-    dropdown.append($('<option></option>').attr('value', value.id).text(value.name));
-
-    $("#lifeSpan").attr('value', key).text(value.life_span);
-   // $( "<img>" ).attr( "src", value.image ).appendTo( "#images" );
+            });
 
 
-   // $("#reportHeader").text(`Information for ${entry.name}` );
-  //  $("#lifeSpan").attr('value', key).text(entry.life_span);
-  //$('#lifeSpan').val(entry.life_span);
-
-  });
-  console.log( data );
-           
-})
-} ;
+        } else {
+            console.log(response.statusText);
+        }
+    });
+}
+getBreeds();
 
 
 
+let saveGeneratedBreads = function (breedSelected) { 
+    let findBreeds = recentBreeds.breeds.some(breed => breed.name === breedSelected); 
+    if (!findBreeds) { 
+        recentBreeds.breeds.push({ 
+            name: breedSelected
+        });
+        
+    }
+    localStorage.setItem("recentBreeds", JSON.stringify(recentBreeds));
+};
+function getBreed(selectedBreed) {
 
 
+    let breedUrl = "https://api.thecatapi.com/v1/breeds/search?q=" + selectedBreed + "&api_key=" + myKey;
 
-getBreed(); 
+    fetch(breedUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                fetch("https://api.thecatapi.com/v1/images/search?breed_id=" + data[0].id + "&limit=1&size=thumb&api_key=" + myKey).then(function (response) {
+                    if (response.ok) {
+                        response.json().then(function (infoOfUrl) {
+
+                            let showBreed = document.getElementById("display")
+                            showBreed.innerHTML = "";
+                            let name = document.createElement("h3");
+                            name.textContent = data[0].name;
+                            
+                            showBreed.appendChild(name);
+                            
+                        })
+                    }
+            })
+        });
+}
+    });
 

9535c4a6f6754196a1fad1b5adf5e63b7c970b6c
YesratRahman <yr2818@vt.edu>
2021-05-26 09:02:56 -0400

    Project Euler Problems

---
+++ ProjectEuler/Problem5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Problem5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < int.MaxValue; i++)
+            {
+                bool isDivisible = true;
+                for (int j = 20; j > 10; j--)
+                {
+                    if (i % j != 0)
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+
+                if (isDivisible)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+        }
+    }
+}

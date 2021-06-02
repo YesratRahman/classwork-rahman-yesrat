02881a1251129afd50169cf20c8684981fb945dd
YesratRahman <yr2818@vt.edu>
2021-06-02 14:39:53 -0400

    Heap Solved

--- GenericsDemo/GenericHeap/MinHeap.cs
+++ GenericsDemo/GenericHeap/MinHeap.cs
@@ -42,6 +42,100 @@ namespace GenericHeap
             SiftUp();
         }
 
-        
+        public void Remove(T toRemove)
+        {
+            int index = _allElements.IndexOf(toRemove);
+            int lastIndex = _allElements.Count - 1;
+            _allElements[index] = _allElements[lastIndex];
+            _allElements.RemoveAt(lastIndex);
+            SiftUp(); 
+        }
+
+
+        public T RemoveMin()
+        {
+            //we're looking for a CompareTo() == 0
+            T value = _allElements[0];
+            _allElements[0] = _allElements[_allElements.Count - 1];
+            _allElements.RemoveAt(_allElements.Count - 1);
+
+            int parentIndex  = 0;
+            
+
+            while(parentIndex < _allElements.Count)
+            {
+                int min = parentIndex;
+                int leftChildIndex = parentIndex * 2 + 1;
+                int rightChildIndex = parentIndex * 2 + 2;
+               
+                if (leftChildIndex < _allElements.Count && _allElements[leftChildIndex].CompareTo(_allElements[min]) == -1)
+                {
+                    min = leftChildIndex; 
+                }
+
+                if (rightChildIndex < _allElements.Count && _allElements[rightChildIndex].CompareTo(_allElements[min]) == -1)
+                {
+                    min = rightChildIndex;
+                }
+
+                if (min == parentIndex) break;
+                else
+                {
+                    Swap(parentIndex, min);
+                }
+
+            }
+
+            return value;
+
+            
+        }
+
+        public T Peek()
+        {
+            if (_allElements.Count > 0) return _allElements[0];
+            return default(T);
+        }
+
+        public T Pop()
+        {
+            T toReturn = default(T);
+            if (_allElements.Count > 0)
+            {
+
+                toReturn = _allElements[0];
+                Remove(toReturn); 
+            } 
+
+            //todo: actually remove the top node
+            //swap with the last element
+            //remove the last element
+            //heapify()
+
+            return toReturn;
+        }
+
+        private void SiftUp()
+        {
+            for(int i = _allElements.Count -1; i > 0; i--)
+            {
+                int parentIndex = (i + 1) / 2 - 1;
+
+                parentIndex = parentIndex >= 0 ? parentIndex : 0;
+                if (_allElements[parentIndex].CompareTo(_allElements[i]) > 1)
+                {
+                    Swap(parentIndex, i); 
+                }
+
+            }
+        }
+
+
+        private void Swap(int first, int second)
+        {
+            T temp = _allElements[first];
+            _allElements[first] = _allElements[second];
+            _allElements[second] = temp;
+        }
     }
 }

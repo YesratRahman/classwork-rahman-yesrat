dc04843f8bb714a2ff86ada547af2cbe265237fb
YesratRahman <yr2818@vt.edu>
2021-06-16 16:58:55 -0400

    Movie Collection

---
+++ MovieCollection/MovieCollection/Controllers/MovieCollectionController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MovieCollection.Models;
+using MovieCollection.Services;
+
+namespace MovieCollection.Controllers
+{
+    public class MovieCollectionController : Controller
+    {
+
+        MovieService _service = new MovieService();
+
+        [HttpGet]
+        public IActionResult EditMovieView(int? id)
+        {
+            if(id != null)
+            {
+                MovieModel model = _service.GetById(id.Value);
+                return View(model); 
+            }
+
+            return this.BadRequest(); 
+        }
+
+        [HttpPost]
+        public IActionResult EditMovieView(MovieModel updateMovie)
+        {
+            _service.EditMovie(updateMovie);
+            return this.RedirectToAction("Index"); 
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IEnumerable<MovieModel> toDisplay = _service.GetAllMovies();
+            return View(toDisplay); 
+        }
+
+        [HttpPost]
+        public IActionResult AddMovieView(MovieModel newMovie)
+        {
+             _service.AddMovie(newMovie);
+
+            return this.RedirectToAction("Index"); 
+        }
+
+        
+
+    }
+}
---
+++ MovieCollection/MovieCollection/Models/ErrorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+
+namespace MovieCollection.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
